d09e5cce9dd217a9cfe8f7fe597581da480565bc
Masaya Sato <sato.masaya.dev@gmail.com>
2022-02-01 16:49:18 +0900

    Avoid redefining local variables

--- aizu_online_judge/courses/alds/2_b.c
+++ aizu_online_judge/courses/alds/2_b.c
@@ -32,12 +32,12 @@ void print(int A[], int N) {
 
 /* Perform a selection sort on an array */
 int selectionSort(int A[], int N) {
-  int i, j, min_j, tmp;
+  int min_j, tmp;
   int num_swapped = 0;
 
   for (int i = 0; i < N - 1; i++) {
     min_j = i;
-    for (j = i; j < N; j++) {
+    for (int j = i; j < N; j++) {
       if (A[j] < A[min_j]) min_j = j;
     }
     tmp = A[i]; A[i] = A[min_j]; A[min_j] = tmp;
