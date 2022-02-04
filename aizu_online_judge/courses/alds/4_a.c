02f28fa24613c07fc16a04ac1ec06be5743d34e6
Masaya Sato <sato.masaya.dev@gmail.com>
2022-02-04 18:43:47 +0900

    Solve exercise problem in Lesson-ALDS1

---
+++ aizu_online_judge/courses/alds/4_a.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int linearSearch(int S[], int n, int key);
+
+/**
+ * https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_A
+ */
+int main() {
+  int i, n, q, key;
+  int S[10000+1];
+  int sum = 0;
+
+  scanf("%d", &n);
+  for (i = 0; i < n; i++) scanf("%d", &S[i]);
+
+  scanf("%d", &q);
+  for (i = 0; i < q; i++) {
+    scanf("%d", &key);
+    if (linearSearch(S, n, key)) sum++;
+  }
+
+  printf("%d\n", sum);
+}
+
+/* Perform a linear search on an array */
+int linearSearch(int S[], int n, int key) {
+  int i = 0;
+
+  S[n] = key;
+
+  while (S[i] != key) i++;
+
+  // Return true if not equal, equivalently key found in array
+  return i != n;
+}
---
+++ aizu_online_judge/courses/alds/4_a.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+static const int MAX = 10000;
+
+int linearSearch(int S[], int n, int key);
+
+/**
+ * https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_A
+ */
+int main() {
+  int i, n, q, key;
+  int S[MAX+1];
+  int sum = 0;
+
+  cin >> n;
+  for (i = 0; i < n; i++) cin >> S[i];
+
+  cin >> q;
+  for (i = 0; i < q; i++) {
+    cin >> key;
+    if (linearSearch(S, n, key)) sum++;
+  }
+
+  cout << sum << endl;
+}
+
+/* Perform a linear search on an array */
+int linearSearch(int S[], int n, int key) {
+  int i = 0;
+
+  S[n] = key;
+
+  while (S[i] != key) i++;
+
+  // Return true if not equal, equivalently key found in array
+  return i != n;
+}
